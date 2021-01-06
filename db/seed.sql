c398d73c33808e099dd3fa80be9bfacdca773966
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-01-06 07:54:50 -0800

    Database executing and tethered

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Ultimate Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Six-Dollar Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jumbo Jack', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sourdough Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Decker Burger', false);
