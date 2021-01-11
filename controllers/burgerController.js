02232c21134a53295d5239922fda4a15ddc3bac9
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-01-11 01:19:35 -0800

    Committed a 'delete' router.

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -21,17 +21,16 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
+  burger.insertOne(
+    ["burger_name", "devoured"], 
+    [req.body.burger_name, req.body.devoured], 
+    function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId})
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -43,11 +42,25 @@ router.put("/api/burgers/:id", function(req, res) {
   condition, function(result) {
     if (result.changedRows === 0 ) {
       return res.status(404).end();
-    }
+    } else {
       res.status(200).end();
-    
+    }
   });
 });
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.deleteOne(condition, function(result) {
+      if (result.changedRows === 0 ) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+  });
+})
+
 
 // Export routes for server.js to use.
 module.exports = router;
