6b9d416972efe80d9f330f2cfb5a7ca06b9d1385
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-01-11 01:18:49 -0800

    Commitetd a 'deleteOne' function

--- config/orm.js
+++ config/orm.js
@@ -72,12 +72,14 @@ function printQuestionMarks(num) {
     },
     // An example of objColVals would be {name: panther, sleepy: true}
     updateOne: function(table, objColVals, condition, cb) {
-      var queryString = "UPDATE " + table;
-  
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
+      var queryString = 
+      
+      "UPDATE " + 
+      table + 
+      " SET " +
+      objToSql(objColVals) +
+      " WHERE " +
+      condition;
   
       console.log(queryString);
       connection.query(queryString, function(err, result) {
@@ -88,19 +90,21 @@ function printQuestionMarks(num) {
         cb(result);
       });
     },
-    // delete: function(table, condition, cb) {
-    //   var queryString = "DELETE FROM" + table;
-    //   queryString += "WHERE";
-    //   queryString += condition;
+    deleteOne: function(table, condition, cb) {
+      var queryString = 
+      "DELETE FROM" + 
+      table +
+      "WHERE" +
+      condition;
   
-    //   connection.query(queryString, function (err, result){
-    //     if(err) {
-    //     throw err;
-    //     }
+      connection.query(queryString, function (err, result){
+        if(err) {
+        throw err;
+        }
         
-    //     cb(result);
-    //   });
-    // }
+        cb(result);
+      });
+    }
   };
   
 
